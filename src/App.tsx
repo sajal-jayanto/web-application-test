b497f537a554eef6d45a21e00af125ab60aa220b
sajal-jayanto <sajaljayanto007@gmail.com>
2025-07-24 22:40:04 +0600

    add router axios client and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Timeline from './pages/Timeline'
+
+const queryClient = new QueryClient()
 
 function App() {
-  const [data, setData] = useState<any>(null)
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.post('/api/postTest')
-        console.log(res.data)
-        setData(res.data)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-    
-    fetchData()
-  }, [])
-
   return (
-    <div>
-      <h1>Display the data obtained from API here</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Timeline />} />
+          {/* <Route path="/murmur/:id" element={<MurmurDetail />} />
+          <Route path="/user/:userId" element={<UserProfile />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
