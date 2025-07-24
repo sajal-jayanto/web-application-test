a76a8112c40c4f573aae907e21f3091e7777b1f7
sajal-jayanto <sajaljayanto007@gmail.com>
2025-07-24 17:02:10 +0600

    create users and mourmur module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3306,
+      port: 3307,
       username: 'docker',
       password: 'docker',
       database: 'test',
@@ -17,8 +18,9 @@ import { User } from './entities/user.entity';
       synchronize: true,
     }),
     TypeOrmModule.forFeature([User]),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
